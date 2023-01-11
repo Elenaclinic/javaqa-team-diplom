91396e24c6033d9311a662d0f124acd35b62727a
Евгения <116790657+Oife88@users.noreply.github.com>
2023-01-11 12:23:53 +0300

    Update GameStore.java

--- src/main/java/ru/netology/GameStore.java
+++ src/main/java/ru/netology/GameStore.java
@@ -1,78 +1,84 @@
 package ru.netology;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class GameStore {
-    private List<Game> games = new ArrayList<>();
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
-    /**
-     * Информация о том, какой игрок сколько играл в игры этого каталога
-     * Ключ - имя игрока
-     * Значение - суммарное количество часов в игры этого каталога
-     */
-    private Map<String, Integer> playedTime = new HashMap<>();
 
-    /**
-     * Создание объекта игры с заданными заголовком и жанром
-     * Каждый объект игры помнит объект каталога, которому она принадлежит
-     */
-    public Game publishGame(String title, String genre) {
-        Game game = new Game(title, genre, this);
-        games.add(game);
-        return game;
-    }
+    public class GameStoreTest {
 
-    /**
-     * Проверяет наличие игры в каталоге и возврашает true
-     * если игра есть и false иначе
-     */
-    public boolean containsGame(Game game) {
-        for (int i = 1; i < games.size(); i++) {
-            if (games.get(i - 1).equals(game)) {
-                return true;
-            }
+
+        @Test
+        public void shouldAddGame() {
+
+            GameStore store = new GameStore();
+            Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+            assertTrue(store.containsGame(game));
         }
-        return false;
-    }
 
-    /**
-     * Регистрирует количество времени, которое проиграл игрок
-     * за игрой этого каталога. Игрок задаётся по имени. Время должно
-     * суммироваться с прошлым значением для этого игрока
-     */
-    public void addPlayTime(String playerName, int hours) {
-        if (playedTime.containsKey(playerName)) {
-            playedTime.put(playerName, playedTime.get(playerName));
-        } else {
-            playedTime.put(playerName, hours);
+
+        @Test
+        public void shouldContainsGame() {
+
+            GameStore store = new GameStore();
+            Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+            Boolean expected = true;
+            Boolean actual = store.containsGame(game);
+
+            assertEquals(expected, actual);
         }
-    }
 
-    /**
-     * Ищет имя игрока, который играл в игры этого каталога больше всего
-     * времени. Если игроков нет, то возвращется null
-     */
-    public String getMostPlayer() {
-        int mostTime = 1;
-        String bestPlayer = null;
-        for (String playerName : playedTime.keySet()) {
-            int playerTime = playedTime.get(playerName);
-            if (playerTime > mostTime) {
-                mostTime = playerTime;
-                bestPlayer = playerName;
-            }
+        @Test
+        public void playTimeCheck() {
+
+            GameStore store = new GameStore();
+            Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+            Player player = new Player("Elena");
+            player.installGame(game);
+            player.play(game, 3);
+            store.addPlayTime("Elena", 4);
+
+
+            int expected = 7;
+            int actual = store.getSumPlayedTime();
+
+            assertEquals(expected, actual);
+
         }
-        return bestPlayer;
-    }
 
-    /**
-     * Суммирует общее количество времени всех игроков, проведённого
-     * за играми этого каталога
-     */
-    public int getSumPlayedTime() {
-        return 0;
-    }
-}
+
+        @Test
+        public void shouldSumPlayedTime1() {
+            GameStore store = new GameStore();
+            Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+            Player player1 = new Player("Elena");
+            player1.installGame(game);
+            player1.play(game, 3);
+
+            Player player2 = new Player("Alex");
+            player2.installGame(game);
+            player2.play(game, 4);
+
+
+            int expected = 7;
+            int actual = store.getSumPlayedTime();
+            assertEquals(expected, actual);
+        }
+
+
+
+
+
+        }
+
+
+
+
+
